ad90df92c7f7ea344e31ba839fc6e375c5f29103
Lucas Kammer <lucascomercialk@gmail.com>
2026-04-10 17:25:45 -0300

    create dashboard and route flask

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify 
-
-@app.route('/status')
-def status():
-    return jsonify({"status": "ok", "msg": "Jarvis funcionando"})
-
+from flask import Flask, jsonify, render_template
 
 app =  Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('dashboard.hyml')
 
 @app.route('/')
 def home():
     return "Jarvis está online"
 
-if __name__ == '_main_':
+@app.route('/status')
+def status():
+    return jsonify({"status": "ok", "msg": "Jarvis funcionando"})
+
+if __name__ == '__main__':
     app.run(debug=True)
