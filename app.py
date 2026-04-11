673245c917103800e7a2ed0356c4a5bb83bc190b
Lucas Kammer <lucascomercialk@gmail.com>
2026-04-10 22:49:11 -0300

    Creates Buttons

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, jsonify, render_template
 
 app =  Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('dashboard.hyml')
 
 @app.route('/')
 def home():
-    return "Jarvis está online"
+    return render_template('dashboard.html')
+
 
 @app.route('/status')
 def status():
     return jsonify({"status": "ok", "msg": "Jarvis funcionando"})
+
+@app.route('/abrir')
+def abrir():
+    return jsonify({"msg":"Comando Executado"})
 
 if __name__ == '__main__':
     app.run(debug=True)
